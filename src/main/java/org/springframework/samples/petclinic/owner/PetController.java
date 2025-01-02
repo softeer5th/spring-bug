4f945d002a09729a70c7e6270b8c1e1c683db15e
keemjoonsung <kjs990114@naver.com>
2025-01-02 17:01:41 +0900

    fix: 펫 이름을 입력하면 이미 존재하는 이름이라고 에러가 나는 버그 수정

--- src/main/java/org/springframework/samples/petclinic/owner/PetController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/PetController.java
@@ -97,7 +97,7 @@ class PetController {
 	@PostMapping("/pets/new")
 	public String processCreationForm(Owner owner, @Valid Pet pet, BindingResult result, ModelMap model,
 			RedirectAttributes redirectAttributes) {
-		if (StringUtils.hasText(pet.getName()) && pet.isNew() && owner.getPet(pet.getName(), false) != null) {
+		if (StringUtils.hasText(pet.getName()) && !pet.isNew() && owner.getPet(pet.getName(), false) != null) {
 			result.rejectValue("name", "duplicate", "already exists");
 		}
 		if(pet.getBirthDate()!= null && pet.getBirthDate().isAfter(LocalDate.now())) {
